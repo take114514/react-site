1988e43d65123bcbdb3cfeb8426bf6f7d09c622e
take114514 <maibadilin@gmail.com>
2021-11-03 16:36:39 +0900

    スキル部分作成

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+export const Skills = () =>{
+    return (
+        <div id ="skills">
+            <div className = "container">
+                <div className = "heading">
+                    <h2>Slills</h2>
+                </div>
+                <div className = "skills-container">
+                </div>
+            </div>
+        </div>
+    );
+};
