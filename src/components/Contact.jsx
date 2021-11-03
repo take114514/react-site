a24cf565df566a9bb7d180c6d48208e35c246536
take114514 <maibadilin@gmail.com>
2021-11-03 16:38:23 +0900

    contact新規作成

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+export const Contact = () =>{
+    return (
+        <div id = "contact" className="contact">
+            <div className = "container">
+                <div className = "contact-contents">
+                    <h2>Contactはこちら</h2>
+                    <p className = "description">ご連絡の際はこちらからよろしくお願いします</p>
+                    <a href = "https://twitter.com/ryutake459" className = "btn">Contact us</a>
+                    </div>
+            </div>
+        </div>
+    );
+};
