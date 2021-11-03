c361a8da2f0f6a50bfddcb063b01d15506c271af
take114514 <maibadilin@gmail.com>
2021-11-03 10:49:59 +0900

    header部分新規作成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,30 @@
-export const Header = () => {
-    const componentName = () =>"Header";
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
 
+export const Header = () =>{
     return (
-        // ここはHTMLではなくJSX（Javascript構文の拡張）
-        <div className = "test-wrapper">
-            このコンポーネントの名前は{componentName()}です。
+        <header className = "main-cover" style = {{backgroundImage:`url(${CoverImage})`}}>
+            {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+            <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            {/* 名前と肩書はみなさんのお名前や肩書を自由に入れてください */}
+            <h1 className="title-text">武井 龍八</h1>
+            <h3 className="title-text">webエンジニア</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+    </header>
+  );
 };
