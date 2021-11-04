697d3e284384a607cbee2fe39a02d3cea536d7dc
take114514 <maibadilin@gmail.com>
2021-11-04 16:01:27 +0900

    レンダリングまでの修正

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,32 @@
+import { useEffect,useState } from "react";
+import axios from "axios";
+
 export const Skills = () =>{
+    const[languageList,setLanguagelist]=useState([]);
+    console.log(languageList);
+
+    useEffect(()=>{
+        axios.get('https://api.github.com/users/take114514/repos')
+        .then((response)=>{
+            // プログッラミング言語のデータを取得
+            const languageList = response.data.map(res=>res.language);
+            const contentedLanguageList = generateLanguageCountObj(languageList);
+            setLanguagelist(contentedLanguageList);
+        });
+    },[]);
+
+    const generateLanguageCountObj=(allLanguageList)=>{
+        const notNullLanguageList = allLanguageList.filter(language=>language!=null);
+        const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+        return uniqueLanguageList.map(item=>{
+            return {
+                language:item,
+                count:allLanguageList.filter(language=>language===item).length
+            }
+        });
+    };
+
     return (
         <div id ="skills">
             <div className = "container">
