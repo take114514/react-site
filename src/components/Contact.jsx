4f7f97d7c1fff1f7d7873de94b0430f03795a610
take114514 <maibadilin@gmail.com>
2021-11-05 08:21:23 +0900

    css調整

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,8 @@ export const Contact = () =>{
         <div id = "contact" className="contact">
             <div className = "container">
                 <div className = "contact-contents">
-                    <h2>Contactはこちら</h2>
-                    <p className = "description">ご連絡の際はこちらからお願いします</p>
+                    <h2>Contact</h2>
+                    <p className = "description">ご連絡はこちらからお願いします。<div className=""></div></p>
                     <a href = "https://twitter.com/ryutake459" className = "btn">Contact us</a>
                     </div>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,13 @@ import { FaTwitter, FaGithub } from 'react-icons/fa';
 export const Header = () =>{
     return (
         <header className = "main-cover" style = {{backgroundImage:`url(${CoverImage})`}}>
-            {/* overlayはカバー画像の上に透過して表示される背景要素です */}
             <div className="overlay"></div>
       <div className="container">
         <div className="display-table">
           <div className="display-table-contents">
-            {/* カバー画像 */}
             <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
-            {/* 名前と肩書はみなさんのお名前や肩書を自由に入れてください */}
             <h1 className="title-text">武井 龍八</h1>
+            <h2 className="title-text">-RYUYA TAKEI-</h2>
             <h3 className="title-text">webエンジニア</h3>
             <ul className="social-icons">
               <li className="icon-link">
